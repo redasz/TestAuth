e855ccb6cff68947356f7c5764bb8daab4579f66
redasz <35854344+redasz@users.noreply.github.com>
2022-04-26 12:54:01 +0300

    Update apiConfig.js

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
     b2cScopes: ["https://COMSandbox.onmicrosoft.com/bef75d2f-c589-4038-98b3-68c388ce0fa3/demo.read"],
-    webApi: "http://localhost:6420"
+    webApi: "https://authpage1.z6.web.core.windows.net/"
   };
