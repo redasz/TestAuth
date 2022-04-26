14c2bb198f59727e50bdc387af0801689fb82dff
redasz <35854344+redasz@users.noreply.github.com>
2022-04-26 11:44:35 +0300

    commit on main

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
-    webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+    b2cScopes: ["https://COMSandbox.onmicrosoft.com/bef75d2f-c589-4038-98b3-68c388ce0fa3/demo.read"],
+    webApi: "http://localhost:6420"
   };
--- App/policies.js
+++ App/policies.js
@@ -5,16 +5,12 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1_susi_reset_v2",
-        editProfile: "B2C_1_edit_profile_v2"
+        signUpSignIn: "B2C_1_SignUp_SignIn"
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
-        },
-        editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_edit_profile_v2"
+            authority: "https://COMSandbox.b2clogin.com/COMSandbox.onmicrosoft.com/B2C_1_SignUp_SignIn",
         }
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: "COMSandbox.b2clogin.com"
 }
